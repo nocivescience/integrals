de1c482b14540e3044192e9a3bc75cd6cf63dc39
ricardo <nocive.science@gmail.com>
2024-07-08 03:55:12 -0400

    funciona pero sin la transformación

--- integral.py
+++ integral.py
@@ -1,8 +1,10 @@
 from manim import *
+
 class IntegralsScene(Scene):
-    conf={
+    conf = {
         'funcion': lambda x: np.sin(x),
     }
+
     def construct(self):
         axes = Axes(
             x_range=[-7, 7, 1],
@@ -12,11 +14,30 @@ class IntegralsScene(Scene):
                 'color': GREEN,    
             },
             y_axis_config={
-                # "numbers_to_include": np.arange(-1, 1, .2),
                 'color': RED,    
             },
         )
         self.play(Create(axes))
         funcion = axes.plot(self.conf['funcion'], color=WHITE)
         self.add(funcion)
+
+        start_dx = 1
+        end_dx = 0.1
+        num_steps = 10
+        dx_values = np.linspace(start_dx, end_dx, num_steps)
+
+        for dx in dx_values:
+            rects = axes.get_riemann_rectangles(
+                graph=funcion,
+                x_range=[-7, 7],
+                dx=dx,
+                stroke_width=0.1,
+                fill_opacity=0.5,
+                stroke_color=BLUE,
+            )
+            self.play(Create(rects), run_time=0.5)
+            self.wait(0.5)
+            self.remove(rects)
+
         self.wait()
+
